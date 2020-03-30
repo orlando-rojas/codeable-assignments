3c28b2c35307056684c3e9bfa141e9bc821cd598
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-03-30 11:09:03 -0500

    code refactored

--- module1/week3/ps2-readability/readability.c
+++ module1/week3/ps2-readability/readability.c
@@ -10,34 +10,18 @@ int main(void)
     int palabras = 1;
     int oraciones = 0;
 
-    for (int i = 0; i < strlen(text); i++)
-    {
-       if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
-       {
-           letras++;
-       }
-       else if (text[i] == ' ')
-       {
-           palabras++;
-       }
-       else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-       {
-           oraciones++;
-       }
+    for (int i = 0; i < strlen(text); i++){
+       if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z')) letras++;
+       else if (text[i] == ' ') palabras++;
+       else if (text[i] == '.' || text[i] == '!' || text[i] == '?') oraciones++;
     }
 
-    float grade = 0.0588 * (100 * (float) letras / (float) palabras) - 0.296 * (100 * (float) oraciones / (float) palabras) - 15.8;
-    if (grade < 16 && grade >= 0)
-    {
-        printf("Grade %i\n", (int) round(grade));
-    }
-    else if (grade >= 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else
-    {
-        printf("Before Grade 1\n");
-    }
+    float L = 100 * (float) letras / (float) palabras;
+    float S = 100 * (float) oraciones / (float) palabras;
 
+    float grade = round(0.0588 * L - 0.296 * S - 15.8);
+    
+    if (grade < 16 && grade >= 0)  printf("Grade %i\n", (int) grade);
+    else if (grade >= 16) printf("Grade 16+\n");
+    else printf("Before Grade 1\n");
 }
